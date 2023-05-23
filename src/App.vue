53d5399e64e9d395d5cbf86ff10215520fbe668a
mimifay <90367168+mimifay@users.noreply.github.com>
2023-05-22 19:30:16 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -110,7 +110,7 @@ export default {
         },
         {
           "callsign": "Barghest",
-          "alias": "Cahara Telestra,
+          "alias": "Cahara Telestra",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
