78c708a8f464694aaa5affd9eee1639a8c453cfc
mimifay <90367168+mimifay@users.noreply.github.com>
2023-05-22 20:32:09 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -138,7 +138,7 @@ export default {
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Preulude to Inferno"
+          "mech": "Prelude to Inferno"
         },
         {
           "callsign": "Compadre",
